ff856e0643be1e493f0596c42ef0618cb349ba9a
rxwx <2202542+rxwx@users.noreply.github.com>
2023-01-04 17:26:26 +0000

    Add project files.

---
+++ DCRACKI/Program.cs
@@ -0,0 +1,287 @@
+﻿using CommandLine;
+using SharpDPAPI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.Linq;
+using System.Security.Principal;
+
+namespace DCRACKI
+{
+    internal class Program
+    {
+        static readonly string masterKeyRegex = @"[0-9A-Fa-f]{8}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{12}";
+
+        class GlobalOptions
+        {
+            [Option('m', "masterkey", Required = true, HelpText = "Path to masterkey file.")]
+            public string MasterKeyPath { get; set; }
+
+            [Option('s', "sid", Required = false, HelpText = "SID of user. If no value is given, we will attempt to recover it from the folder name.")]
+            public string Sid { get; set; }
+        }
+
+        [Verb("test", HelpText = "Test a single password or NTLM hash against a masterkey file.")]
+        class TestOptions: GlobalOptions
+        {
+            [Option('p', "password", Required = false, HelpText = "Password or NTLM hash for user.")]
+            public string Password { get; set; }
+
+            [Option('l', "local", Required = false, Default = false, HelpText = "Masterkey is from non Domain Joined machine.")]
+            public bool Local { get; set; }
+        }
+
+        [Verb("preferred", HelpText = "Parse a DPAPI Preferrred file to extract the preferred masterkey GUID.")]
+        class PreferredOptions
+        {
+            [Option('f', "file", Required = true, HelpText = "Path to Peferred file.")]
+            public string PreferredFile { get; set; }
+        }
+
+        [Verb("hash", HelpText = "Dump masterkey hash in john/hashcat format.")]
+        class HashOptions : GlobalOptions
+        {
+            [Option('c', "context", Required = false, Default = 3, 
+                HelpText = "Context. Default is 3 for Domain Joined machines above Windows 10 1607. " +
+                "Use context 2 for older systems (pre-1607)." +
+                "If the machine is non Domain Joined, set the context to 1.")]
+            public int Context { get; set; }
+        }
+
+        [Verb("crack", HelpText = "Attempt to crack a masterkey using a dictionary of passwords or NTLM/SHA1 hashes.")]
+        class CrackOptions: GlobalOptions
+        {
+            [Option('d', "dictionary", Required = true, HelpText = "Dictionary of passwords or NTLM/SHA1 hashes.")]
+            public string Dictionary { get; set; }
+
+            [Option('l', "local", Required = false, Default = false, HelpText = "Masterkey is from non Domain Joined machine.")]
+            public bool Local { get; set; }
+
+            [Option('n', "ntlm", Required = false, Default = false, HelpText = "Dictionary is NTLM hashes (skip hashing).")]
+            public bool IsNtlm { get; set; }
+
+            [Option("sha1", Required = false, Default = false, HelpText = "Dictionary is SHA1 hashes (skip hashing).")]
+            public bool IsSha1 { get; set; }
+        }
+
+        private static Type[] LoadVerbs()
+        {
+            return Assembly.GetExecutingAssembly().GetTypes()
+                .Where(t => t.GetCustomAttribute<VerbAttribute>() != null).ToArray();
+        }
+
+        public static void HandleErrors(object obj)
+        {
+        }
+
+        private static void Run(object obj)
+        {
+            Console.WriteLine();
+            switch (obj)
+            {
+                case TestOptions opt:
+                    TestSingle(opt);
+                    break;
+                case CrackOptions opt:
+                    DictionaryAttack(opt);
+                    break;
+                case PreferredOptions opt:
+                    GetPreferredKey(opt);
+                    break;
+                case HashOptions opt:
+                    DumpHash(opt);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void GetPreferredKey(PreferredOptions opt)
+        {
+            if (!File.Exists(opt.PreferredFile) || !(new FileInfo(opt.PreferredFile).Length == 24))
+                throw new ArgumentException("input file is not valid");
+
+            byte[] guidBytes = new byte[16];
+            using (BinaryReader reader = new BinaryReader(new FileStream(opt.PreferredFile, FileMode.Open)))
+            {
+                reader.Read(guidBytes, 0, 16);
+            }
+            var guid = new Guid(guidBytes).ToString();
+            Console.WriteLine($"[+] Preferred: {guid}");
+        }
+
+        public static string GetSidFromBKFile(string bkFile)
+        {
+            string sid = string.Empty;
+            byte[] bkBytes = File.ReadAllBytes(bkFile);
+
+            if (bkBytes.Length > 28)
+            {
+                try
+                {
+                    SecurityIdentifier sidObj = new SecurityIdentifier(bkBytes, 0x3c);
+                    sid = sidObj.Value;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Failed to parse BK file: {ex.Message}");
+                }
+            }
+            return sid;
+        }
+
+        private static string TryGetSid(string masterKeyPath)
+        {
+            string sid = string.Empty;
+
+            // First check if there is a BK file we can get the SID from
+            foreach (string file in Directory.GetFiles(Path.GetDirectoryName(masterKeyPath), "*", SearchOption.AllDirectories))
+            {
+                if (Path.GetFileName(file).StartsWith("BK-"))
+                {
+                    sid = GetSidFromBKFile(file);
+                    if (!string.IsNullOrEmpty(sid))
+                    {
+                        Console.WriteLine($"[*] Found SID from BK file: {sid}");
+                        break;
+                    }
+                }
+            }
+
+            // Fall back to directory name
+            if (string.IsNullOrEmpty(sid) && Regex.IsMatch(Path.GetDirectoryName(masterKeyPath), 
+                @"^S-\d-\d+-(\d+-){1,14}\d+$", RegexOptions.IgnoreCase))
+            {
+                sid = Path.GetDirectoryName(masterKeyPath);
+                Console.WriteLine($"[*] Found SID from directory: {sid}");
+            }
+            else if (string.IsNullOrEmpty(sid))
+            {
+                Console.WriteLine("Could not determine users's SID. " +
+                    "Ensure that DPAPI Masterkey directory name contains the user SID, " +
+                    "OR that the BK-<NETBIOSDOMAINNAME> file is present");
+            }
+            return sid;
+        }
+
+        private static void TestSingle(TestOptions opt)
+        {
+            if (!Regex.IsMatch(Path.GetFileName(opt.MasterKeyPath), masterKeyRegex) 
+                || !File.Exists(opt.MasterKeyPath))
+            {
+                Console.WriteLine("[!] Provided file does not appear to be a DPAPI Master Key file");
+                return;
+            }
+
+            string sid = opt.Sid;
+            if (string.IsNullOrEmpty(sid))
+                sid = TryGetSid(opt.MasterKeyPath);
+            if (string.IsNullOrEmpty(sid))
+                return;
+
+            bool isNtlm = Regex.IsMatch(opt.Password, "^[a-f0-9]{32}$", RegexOptions.IgnoreCase);
+            bool isSha1 = Regex.IsMatch(opt.Password, "^[a-f0-9]{40}$", RegexOptions.IgnoreCase);
+
+            if (opt.Local && isNtlm && !isSha1)
+            {
+                Console.WriteLine("[!] NTLM Hash provided, but the Masterkey is from a non Domain Joined Machine");
+                Console.WriteLine("[!] This will not work, since SHA1 is used instead of RC4. Use a password or SHA1 instead.");
+                return;
+            }
+
+            if (IsValidKey(opt.MasterKeyPath, sid, opt.Password, isNtlm || isSha1, !opt.Local))
+                Console.WriteLine("[+] {0} is VALID", isNtlm ? "NTLM" : "Password");
+            else
+                Console.WriteLine("[!] {0} is NOT valid", isNtlm ? "NTLM" : "Password");
+        }
+
+        private static void DictionaryAttack(CrackOptions opt)
+        {
+            if (!Regex.IsMatch(Path.GetFileName(opt.MasterKeyPath), masterKeyRegex) || !File.Exists(opt.MasterKeyPath))
+            {
+                Console.WriteLine("[!] Provided file does not appear to be a DPAPI Master Key file");
+                return;
+            }
+
+            string sid = opt.Sid;
+            if (string.IsNullOrEmpty(sid))
+                sid = TryGetSid(opt.MasterKeyPath);
+            if (string.IsNullOrEmpty(sid))
+                return;
+
+            if (opt.IsNtlm && opt.Local)
+            {
+                Console.WriteLine("[!] You have specified that the MasterKey file is from a non Domain Joined machine");
+                Console.WriteLine("[!] Providing NTLM hashes will not work. Provide a list of passwords or SHA1 instead.");
+                return;
+            }
+
+            Console.WriteLine("[*] Running with {0} dictionary", opt.IsNtlm ? "NTLM" : "password");
+            Console.WriteLine("[*] Started at {0}", DateTime.Now);
+
+            foreach (string line in File.ReadLines(opt.Dictionary))
+            {
+                if (IsValidKey(opt.MasterKeyPath, sid, line, opt.IsNtlm || opt.IsSha1, !opt.Local))
+                {
+                    Console.WriteLine("[+] Success! Found {0}: {1}", (opt.IsNtlm || opt.IsSha1) ? "Hash" : "Password", line);
+                    break;
+                }
+            }
+            Console.WriteLine("[*] Finished at {0}", DateTime.Now);
+        }
+
+        private static bool IsValidKey(string masterKeyPath, string sid, string password, 
+            bool pth, bool domain = true)
+        {
+
+            byte[] masterKeyBytes = File.ReadAllBytes(masterKeyPath);
+            byte[] hmacBytes = Dpapi.CalculateKeys(password, sid, pth, domain);
+
+            try
+            {
+                // Try and decrypt masterKey
+                KeyValuePair<string, string> plaintextMasterKey = Dpapi.DecryptMasterKeyWithSha(masterKeyBytes, hmacBytes);
+                Console.WriteLine($"[+] Decrypted: {plaintextMasterKey.Key}:{plaintextMasterKey.Value}");
+                return true;
+            }
+            catch {}
+            return false;
+        }
+        private static void DumpHash(HashOptions opt)
+        {
+            if (!Regex.IsMatch(Path.GetFileName(opt.MasterKeyPath), masterKeyRegex)
+                || !File.Exists(opt.MasterKeyPath))
+            {
+                Console.WriteLine("[!] Provided file does not appear to be a DPAPI Master Key file");
+                return;
+            }
+
+            string sid = opt.Sid;
+            if (string.IsNullOrEmpty(sid))
+                sid = TryGetSid(opt.MasterKeyPath);
+            if (string.IsNullOrEmpty(sid))
+                return;
+
+            string hash = Dpapi.FormatHash(File.ReadAllBytes(opt.MasterKeyPath), sid, opt.Context);
+            Console.WriteLine($"[+] Hash: \n{hash}");
+        }
+
+        static void Main(string[] args)
+        {
+            var types = LoadVerbs();
+
+            try
+            {
+                Parser.Default.ParseArguments(args, types)
+                .WithParsed(Run)
+                .WithNotParsed(HandleErrors);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[!] Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ DCRACKI/SharpDPAPI/lib/Dpapi.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using PBKDF2;
+
+namespace SharpDPAPI
+{
+    public class Dpapi
+    {
+        public static byte[] CalculateKeys(string password, string userSID, 
+            bool pth = false, bool domain = true)
+        {
+            Console.WriteLine($"[*] Is Domain Joined: {domain}");
+            Console.WriteLine($"[*] Pass-the-Hash mode: {pth}");
+
+            var utf16pass = Encoding.Unicode.GetBytes(password);
+            var utf16sid = Encoding.Unicode.GetBytes(userSID);
+
+            var utf16sidfinal = new byte[utf16sid.Length + 2];
+            utf16sid.CopyTo(utf16sidfinal, 0);
+            utf16sidfinal[utf16sidfinal.Length - 2] = 0x00;
+
+            byte[] derived = null;
+            byte[] finalKey = null;
+
+            if (!domain)
+            {
+                if (pth)
+                {
+                    // Pass-the-hash (skip initial SHA1 phase)
+                    // Note: SHA1 hash must be in format: SHA1(UTF16LE(password))
+                    // e.g. from mimikatz' sekurlsa::msv
+                    derived = Helpers.ConvertHexStringToByteArray(password);
+                }
+                else
+                {
+                    //Calculate SHA1 from user password
+                    using (var sha1 = new SHA1Managed())
+                    {
+                        derived = sha1.ComputeHash(utf16pass);
+                    }
+                }
+            }
+            else
+            {
+                //Calculate NTLM from user password. Kerberos's RC4_HMAC key is the NTLM hash
+                string rc4Hash = pth ? password : Crypto.KerberosPasswordHash(Interop.KERB_ETYPE.rc4_hmac, password);
+
+                var ntlm = Helpers.ConvertHexStringToByteArray(rc4Hash);
+
+                //Calculate SHA1 of NTLM from user password
+                byte[] tmpbytes1;
+
+                using (var hMACSHA256 = new HMACSHA256())
+                {
+                    var deriveBytes = new Pbkdf2(hMACSHA256, ntlm, utf16sid, 10000);
+                    tmpbytes1 = deriveBytes.GetBytes(32, "sha256");
+                }
+
+                using (var hMACSHA256 = new HMACSHA256())
+                {
+                    var deriveBytes = new Pbkdf2(hMACSHA256, tmpbytes1, utf16sid, 1);
+                    derived = deriveBytes.GetBytes(16, "sha256");
+                }
+            }
+
+            using (var hmac = new HMACSHA1(derived))
+            {
+                finalKey = hmac.ComputeHash(utf16sidfinal);
+            }
+            return finalKey;
+        }
+
+        private static byte[] DerivePreKey(byte[] shaBytes, int algHash, byte[] salt, int rounds)
+        {
+            byte[] derivedPreKey;
+
+            switch (algHash)
+            {
+                // CALG_SHA_512 == 32782
+                case 32782:
+                    {
+                        // derive the "Pbkdf2/SHA512" key for the masterkey, using MS' silliness
+                        using (var hmac = new HMACSHA512())
+                        {
+                            var df = new Pbkdf2(hmac, shaBytes, salt, rounds);
+                            derivedPreKey = df.GetBytes(48);
+                        }
+
+                        break;
+                    }
+
+                case 32777:
+                    {
+                        // derive the "Pbkdf2/SHA1" key for the masterkey, using MS' silliness
+                        using (var hmac = new HMACSHA1())
+                        {
+                            var df = new Pbkdf2(hmac, shaBytes, salt, rounds);
+                            derivedPreKey = df.GetBytes(32);
+                        }
+
+                        break;
+                    }
+
+                default:
+                    throw new Exception($"alg hash  '{algHash} / 0x{algHash:X8}' not currently supported!");
+            }
+
+            return derivedPreKey;
+        }
+
+        private static bool IsValidHMAC(byte[] plaintextBytes, byte[] masterKeyFull, byte[] shaBytes, Type HMACType)
+        {
+            var obj = (HMAC)Activator.CreateInstance(HMACType);
+            var HMACLen = obj.HashSize / 8;
+
+            // we're HMAC'ing the first 16 bytes of the decrypted buffer with the shaBytes as the key
+            var hmacSalt = new byte[16];
+            Array.Copy(plaintextBytes, hmacSalt, 16);
+
+            var hmac = new byte[HMACLen];
+            Array.Copy(plaintextBytes, 16, hmac, 0, hmac.Length);
+
+            var hmac1 = (HMAC)Activator.CreateInstance(HMACType, shaBytes);
+            var round1Hmac = hmac1.ComputeHash(hmacSalt);
+
+            // round 2
+            var hmac2 = (HMAC)Activator.CreateInstance(HMACType, round1Hmac);
+            var round2Hmac = hmac2.ComputeHash(masterKeyFull);
+
+            // compare the second HMAC value to the original plaintextBytes, starting at index 16
+            if (hmac.SequenceEqual(round2Hmac))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static byte[] DecryptTripleDESHmac(byte[] shaBytes, byte[] final, byte[] encData)
+        {
+            var desCryptoProvider = new TripleDESCryptoServiceProvider();
+
+            var ivBytes = new byte[8];
+            var key = new byte[24];
+
+            Array.Copy(final, 24, ivBytes, 0, 8);
+            Array.Copy(final, 0, key, 0, 24);
+
+            desCryptoProvider.Key = key;
+            desCryptoProvider.IV = ivBytes;
+            desCryptoProvider.Mode = CipherMode.CBC;
+            desCryptoProvider.Padding = PaddingMode.Zeros;
+
+            var plaintextBytes = desCryptoProvider.CreateDecryptor().TransformFinalBlock(encData, 0, encData.Length);
+            var masterKeyFull = new byte[64];
+            Array.Copy(plaintextBytes, plaintextBytes.Length - masterKeyFull.Length, masterKeyFull, 0, masterKeyFull.Length);
+
+            using (var sha1 = new SHA1Managed())
+            {
+                var masterKeySha1 = sha1.ComputeHash(masterKeyFull);
+
+                if (!IsValidHMAC(plaintextBytes, masterKeyFull, shaBytes, typeof(HMACSHA1)))
+                    throw new Exception("HMAC integrity check failed!");
+
+                return masterKeySha1;
+            }
+        }
+
+        private static byte[] DecryptAes256HmacSha512(byte[] shaBytes, byte[] final, byte[] encData)
+        {
+            var aesCryptoProvider = new AesManaged();
+
+            var ivBytes = new byte[16];
+            Array.Copy(final, 32, ivBytes, 0, 16);
+
+            var key = new byte[32];
+            Array.Copy(final, 0, key, 0, 32);
+
+            aesCryptoProvider.Key = key;
+            aesCryptoProvider.IV = ivBytes;
+            aesCryptoProvider.Mode = CipherMode.CBC;
+            aesCryptoProvider.Padding = PaddingMode.Zeros;
+
+            // decrypt the encrypted data using the Pbkdf2-derived key
+            var plaintextBytes = aesCryptoProvider.CreateDecryptor().TransformFinalBlock(encData, 0, encData.Length);
+            var masterKeyFull = new byte[64];
+            Array.Copy(plaintextBytes, plaintextBytes.Length - masterKeyFull.Length, masterKeyFull, 0, masterKeyFull.Length);
+
+            using (var sha1 = new SHA1Managed())
+            {
+                var masterKeySha1 = sha1.ComputeHash(masterKeyFull);
+
+                if (!IsValidHMAC(plaintextBytes, masterKeyFull, shaBytes, typeof(HMACSHA512)))
+                    throw new Exception("HMAC integrity check failed!");
+
+                return masterKeySha1;
+            }
+        }
+
+        public static byte[] GetMasterKey(byte[] masterKeyBytes)
+        {
+            // helper to extract domain masterkey subbytes from a master key blob
+
+            var offset = 96;
+
+            var masterKeyLen = BitConverter.ToInt64(masterKeyBytes, offset);
+            offset += 4 * 8; // skip the key length headers
+
+            var masterKeySubBytes = new byte[masterKeyLen];
+            Array.Copy(masterKeyBytes, offset, masterKeySubBytes, 0, masterKeyLen);
+
+            return masterKeySubBytes;
+        }
+
+        public static string FormatHash(byte[] masterKeyBytes, string sid, int context = 3)
+        {
+            var mkBytes = GetMasterKey(masterKeyBytes);
+
+            var offset = 4;
+            var salt = new byte[16];
+            Array.Copy(mkBytes, 4, salt, 0, 16);
+            offset += 16;
+
+            var rounds = BitConverter.ToInt32(mkBytes, offset);
+            offset += 4;
+
+            var algHash = BitConverter.ToInt32(mkBytes, offset);
+            offset += 4;
+
+            var algCrypt = BitConverter.ToInt32(mkBytes, offset);
+            offset += 4;
+
+            var encData = new byte[mkBytes.Length - offset];
+            Array.Copy(mkBytes, offset, encData, 0, encData.Length);
+
+            int version = 0;
+            string cipherAlgo;
+            string hmacAlgo;
+
+            switch (algCrypt)
+            {
+                case 26128 when (algHash == 32782 || algHash == 32772):
+                    version = 2;
+                    cipherAlgo = "aes256";
+                    hmacAlgo = "sha512";
+                    break;
+                case 26115 when (algHash == 32777):
+                    version = 1;
+                    cipherAlgo = "des3";
+                    hmacAlgo = "sha1";
+                    break;
+                default:
+                    throw new Exception($"Alg crypt '{algCrypt} / 0x{algCrypt:X8}' not currently supported!");
+            }
+
+            string hash = string.Format(
+                "$DPAPImk${0}*{1}*{2}*{3}*{4}*{5}*{6}*{7}*{8}",
+                version,
+                context,
+                sid,
+                cipherAlgo,
+                hmacAlgo,
+                rounds,
+                Helpers.ByteArrayToString(salt),
+                encData.Length * 2,
+                Helpers.ByteArrayToString(encData)
+                );
+            return hash;
+        }
+
+
+        public static KeyValuePair<string, string> DecryptMasterKeyWithSha(byte[] masterKeyBytes, byte[] shaBytes)
+        {
+            // takes masterkey bytes and SYSTEM_DPAPI masterkey sha bytes, returns a dictionary of guid:sha1 masterkey mappings
+            var guidMasterKey = $"{{{Encoding.Unicode.GetString(masterKeyBytes, 12, 72)}}}";
+
+            var mkBytes = GetMasterKey(masterKeyBytes);
+
+            var offset = 4;
+            var salt = new byte[16];
+            Array.Copy(mkBytes, 4, salt, 0, 16);
+            offset += 16;
+
+            var rounds = BitConverter.ToInt32(mkBytes, offset);
+            offset += 4;
+
+            var algHash = BitConverter.ToInt32(mkBytes, offset);
+            offset += 4;
+
+            var algCrypt = BitConverter.ToInt32(mkBytes, offset);
+            offset += 4;
+
+            var encData = new byte[mkBytes.Length - offset];
+            Array.Copy(mkBytes, offset, encData, 0, encData.Length);
+
+            var derivedPreKey = DerivePreKey(shaBytes, algHash, salt, rounds);
+
+            switch (algCrypt)
+            {
+                // CALG_AES_256 == 26128 , CALG_SHA_512 == 32782
+                case 26128 when (algHash == 32782|| algHash == 32772):
+                    {
+                        var masterKeySha1 = DecryptAes256HmacSha512(shaBytes, derivedPreKey, encData);
+                        var masterKeyStr = BitConverter.ToString(masterKeySha1).Replace("-", "");
+
+                        return new KeyValuePair<string, string>(guidMasterKey, masterKeyStr);
+                    }
+
+                // Support for 32777(CALG_HMAC) / 26115(CALG_3DES)
+                case 26115 when (algHash == 32777):
+                    {
+                        var masterKeySha1 = DecryptTripleDESHmac(shaBytes, derivedPreKey, encData);
+                        var masterKeyStr = BitConverter.ToString(masterKeySha1).Replace("-", "");
+
+                        return new KeyValuePair<string, string>(guidMasterKey, masterKeyStr);
+                    }
+
+                default:
+                    throw new Exception($"Alg crypt '{algCrypt} / 0x{algCrypt:X8}' not currently supported!");
+            }
+
+        }
+    }
+}
